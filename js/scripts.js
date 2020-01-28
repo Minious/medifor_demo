5af7b40a18023a9b1212616ba0286d3f815adc87
Baptiste CHOCOT <Chocorean@users.noreply.github.com>
2020-01-28 16:57:56 +0100

    1st step- rework (#1)
    * add zoom and select buttons when hover; slightly modify halos' look
    
    * Bugfix for Safari browsers ; Remove unused code
    
    * Move irrelevant js code into css
    
    * turn LF into CRLF...
    
    * debut calcul score et affichage apres submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+let scores = [];
+
 function randInt(min, max){
     return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -9,10 +11,54 @@ function shuffle(array) {
     }
 }
 
+function shuffleImages() {
+    // TODO
+}
+
+function buildScoreDiv() {
+    /*
+     * Idees:
+     - ne l'afficher qu'a la fin des images
+     - afficher les 3 scores en cascade puis un total ; dessiner une grille ?
+     -> introduire la suite
+     */
+    let darkDiv = document.createElement('div');
+    darkDiv.classList.add('darkFrontDiv');
+    darkDiv.style.opacity = '0.7';
+
+    let scoreDiv = document.createElement('div');
+    scoreDiv.classList.add('scoreDiv');
+
+    let p0 = document.createElement('p');
+    p0.innerText = 'Score #0: ' + scores[0] + '/2';
+    let p1 = document.createElement('p');
+    p1.innerText = 'Score #1: ' + scores[1] + '/4';
+    let p2 = document.createElement('p');
+    p2.innerText = 'Score #2: ' + scores[2] + '/9';
+    let p3 = document.createElement('p');
+    p3.innerText = 'Score #3: ' + scores[3] + '/16';
+
+    let pBlank = document.createElement('p');
+    
+    let total = scores[0] + scores[1] + scores[2] + scores[3];
+    let accuracy = Math.round(total / 31 * 10000) / 100;
+    let pTotal = document.createElement('p');
+    pTotal.innerText = 'Global accuracy: ' + accuracy + '%';
+
+    scoreDiv.appendChild(p0);
+    scoreDiv.appendChild(p1);
+    scoreDiv.appendChild(p2);
+    scoreDiv.appendChild(p3);
+    scoreDiv.appendChild(pBlank);
+    scoreDiv.appendChild(pTotal);
+    darkDiv.appendChild(scoreDiv);
+    document.body.appendChild(darkDiv);
+}
+
 function createPageManipulated(data, nbImages){
     return new Promise(function(resolve, reject) {
-        // console.log(data)
-
+        document.body.style.backgroundImage = "none";
+        
         let manipulatedImages = data.filter(imageData => imageData.manipulated);
         let notManipulatedImages = data.filter(imageData => !imageData.manipulated);
 
@@ -28,7 +74,7 @@ function createPageManipulated(data, nbImages){
         let headerTag = document.createElement("div");
         headerTag.className = "header";
         let questionTag = document.createElement("p");
-        questionTag.innerText = "Which one has been manipulated?"
+        questionTag.innerText = "Which one have been manipulated?"
         headerTag.appendChild(questionTag);
         globalContainer.appendChild(headerTag);
 
@@ -37,37 +83,33 @@ function createPageManipulated(data, nbImages){
         let submitButton = document.createElement("button");
         submitButton.innerText = "Submit";
         submitButton.addEventListener('click', function (e) {
-            // console.log(document.getElementsByClassName("imagesContainer")[0])
-            // console.log(document.getElementsByClassName("imagesContainer")[0].childNodes)
+            /* TODO
+            - gradient + bouger les images suivant le score intermediaire
+            document.body.style.backgroundImage = "linear-gradient(to right, green, rgb(78, 90, 102), red)";
+            - 
+            - puis scores
+             */
+            let imgQty = document.getElementsByClassName("imagesContainer")[0].childNodes.length;
+            let currentScore = 0;
             Array.from(document.getElementsByClassName("imagesContainer")[0].childNodes).map(div => div.childNodes[0]).forEach(el => {
-                // console.log(el)
                 let isSelected = !!el.selectedImage;
                 let isManipulated = !!el.manipulated;
-                // console.log(el.idx, isSelected, isManipulated)
-                if(isSelected != isManipulated)
-                    el.style.boxShadow = "0 0 20px 3px rgba(255, 0, 0, 1)";
-                else
-                    el.style.boxShadow = "0 0 20px 3px rgba(0, 255, 0, 1)";
-
-                // window.setTimeout(main, 2000);
-                resolve();
+                currentScore += (isSelected == isManipulated);
+                // el.style.boxShadow = (isSelected != isManipulated ? "0 0 20px 3px rgba(255, 0, 0, 1)" : "0 0 20px 3px rgba(0, 255, 0, 1)");
             });
+            scores.push(currentScore);
+            shuffleImages(currentScore, imgQty);
+            resolve();
         });
         footerTag.appendChild(submitButton);
         globalContainer.appendChild(footerTag);
 
         let nbImagesColumns = Math.ceil(Math.sqrt(nbImages));
-        let nbImagesRows = Math.ceil(nbImages / nbImagesColumns);
-        let windowWidth = window.innerWidth;
-        let windowHeight = window.innerHeight;
-        let ratioImage = 1.3;
         let widthImageCell = 200;
         let gapCell = 10;
 
         let imagesContainerTag = document.createElement("div");
         imagesContainerTag.className = "imagesContainer";
-        // imagesContainerTag.className = "imagesContainer";
-        imagesContainerTag.style.display = "grid";
         imagesContainerTag.style.gridGap = gapCell + "px";
         imagesContainerTag.style.width = (nbImagesColumns * widthImageCell + (nbImagesColumns - 1) * gapCell) + "px";
         imagesContainerTag.style.gridTemplateColumns = "1fr ".repeat(nbImagesColumns);
@@ -77,8 +119,6 @@ function createPageManipulated(data, nbImages){
         imagesToAppend = imagesToAppend.concat(createImagesTags(notManipulatedImages, nbNotManipulatedImages, widthImageCell));
         shuffle(imagesToAppend)
         imagesToAppend.forEach(imageToAppend => imagesContainerTag.appendChild(imageToAppend));
-
-        // console.log(footerTag.clientHeight)
 
         globalContainer.insertBefore(imagesContainerTag, footerTag);
     });
@@ -90,23 +130,74 @@ function createImagesTags(imageArray, nbImages, widthImageCell){
         let indexImage = randInt(0, imageArray.length);
 
         let imageCellTag = document.createElement("div");
+        imageCellTag.classList.add('imgCell');
         imageCellTag.style.width = widthImageCell + "px";
 
         let imageTag = document.createElement("img");
+        imageTag.classList.add('exImg');
         imageTag.style.width = widthImageCell * 0.8 + "px";
+        imageTag.style.maxHeight = imageTag.style.width;
         imageTag.src = "images/" + imageArray[indexImage].filename;
         imageTag.manipulated = imageArray[indexImage].manipulated;
         imageTag.idx = indexImage;
-        imageTag.addEventListener('click', function (e) {
-            e.target.selectedImage = !e.target.selectedImage;
-            // console.log(e.target.selectedImage)
-            if(e.target.selectedImage)
-                e.target.style.boxShadow = "0 0 20px 3px rgba(255, 255, 255, 0.3)";
-            else
-                e.target.style.boxShadow = "0 0 20px 3px rgba(0, 0, 0, 0.3)";
-        });
+
         imageCellTag.appendChild(imageTag);
         imagesCellsTags.push(imageCellTag);
+
+        let absoluteBox = document.createElement('div');
+        absoluteBox.classList.add('absBox');
+        absoluteBox.style.width = imageCellTag.style.width;
+
+        // zoomImg and tickImg are squares.
+        let zoomImg = document.createElement('img');
+        zoomImg.classList.add('hoverImg');
+        zoomImg.src = 'assets/zoom-in.png';
+        zoomImg.style.width = widthImageCell * 0.2 + "px";
+        zoomImg.style.height = zoomImg.style.width;
+        zoomImg.style.marginLeft = widthImageCell * 0.2 + "px";
+        let tickImg = document.createElement('img');
+        tickImg.classList.add('hoverImg');
+        tickImg.src = 'assets/tick.png';
+        tickImg.style.width = widthImageCell * 0.2 + "px";
+        tickImg.style.height = tickImg.style.width;
+        tickImg.style.marginLeft = widthImageCell * 0.2 + "px";
+        // functions
+        zoomImg.addEventListener('click', function (e) {
+            let zoomDiv = document.createElement('div');
+            zoomDiv.classList.add('darkFrontDiv')
+
+            let zoomedPic = document.createElement('img');
+            zoomedPic.src = imageTag.src;
+
+            zoomDiv.addEventListener('click', function (e) {
+                document.body.removeChild(zoomDiv);
+            });
+
+            zoomDiv.appendChild(zoomedPic);
+            document.body.appendChild(zoomDiv);
+        });
+        tickImg.addEventListener('click', function (e) {
+            imageTag.selectedImage = !imageTag.selectedImage;
+            if (imageTag.selectedImage) {
+                imageTag.style.boxShadow = "0 0 20px 5px rgba(255, 255, 255, 0.7)";
+            } else {
+                imageTag.style.boxShadow = "0 0 20px 5px rgba(0, 0, 0, 0.6)";
+            }
+        });
+        absoluteBox.appendChild(zoomImg);
+        absoluteBox.appendChild(tickImg);
+
+        imageCellTag.addEventListener('mouseenter', function (e) {
+            // if outside of the listener, img height is not defined yet --> jquery pls
+            let buttonsTopMargin = imageCellTag.clientHeight / 2  - zoomImg.style.width.slice(0, -2) / 2 + 'px';
+            zoomImg.style.marginTop = buttonsTopMargin;
+            tickImg.style.marginTop = buttonsTopMargin;
+            // ---
+            imageCellTag.appendChild(absoluteBox);
+        });
+        imageCellTag.addEventListener('mouseleave', function (e) {
+            imageCellTag.removeChild(absoluteBox);
+        });
     }
     return imagesCellsTags;
 }
@@ -143,7 +234,11 @@ function main(){
             return new Promise((resolve, reject) => {
                 setTimeout(() => resolve(), 2000);
             })
-        }).then(() => alert('DONE'));
+        }).then(
+            // TODO
+            // - display final score in a cool way
+            () => alert('DONE')
+        );
     });
 }
 
