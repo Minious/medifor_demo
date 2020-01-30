b34f303ded4e5f0d969a976239793c98e0972877
Godard <eng6@PN101915.campus.nist.gov>
2020-01-30 16:00:04 -0500

    display images as a grid with relative positionning for animations later on

--- js/scripts.js
+++ js/scripts.js
@@ -58,9 +58,6 @@ function buildScoreDiv() {
 function createPageManipulated(data, nbImages){
 	return new Promise(function(resolve, reject) {
 		document.body.style.backgroundImage = "none";
-		
-		let manipulatedImages = data.filter(imageData => imageData.manipulated);
-		let notManipulatedImages = data.filter(imageData => !imageData.manipulated);
 
 		let nbImageTypeOffset = Math.ceil(nbImages * 0.2)
 		let nbManipulatedImages = randInt(nbImageTypeOffset, nbImages - nbImageTypeOffset);
@@ -91,7 +88,7 @@ function createPageManipulated(data, nbImages){
 			 */
 			let imgQty = document.getElementsByClassName("imagesContainer")[0].childNodes.length;
 			let currentScore = 0;
-			Array.from(document.getElementsByClassName("imagesContainer")[0].childNodes).map(div => div.childNodes[0]).forEach(el => {
+			Array.from(document.getElementsByClassName("imagesContainer")[0].childNodes).forEach(el => {
 				let isSelected = !!el.selectedImage;
 				let isManipulated = !!el.manipulated;
 				currentScore += (isSelected == isManipulated);
@@ -110,96 +107,110 @@ function createPageManipulated(data, nbImages){
 
 		let imagesContainerTag = document.createElement("div");
 		imagesContainerTag.className = "imagesContainer";
-		imagesContainerTag.style.gridGap = gapCell + "px";
-		imagesContainerTag.style.width = (nbImagesColumns * widthImageCell + (nbImagesColumns - 1) * gapCell) + "px";
-		imagesContainerTag.style.gridTemplateColumns = "1fr ".repeat(nbImagesColumns);
-
-		let imagesToAppend = [];
-		imagesToAppend = imagesToAppend.concat(createImagesTags(manipulatedImages, nbManipulatedImages, widthImageCell));
-		imagesToAppend = imagesToAppend.concat(createImagesTags(notManipulatedImages, nbNotManipulatedImages, widthImageCell));
-		shuffle(imagesToAppend)
-		imagesToAppend.forEach(imageToAppend => imagesContainerTag.appendChild(imageToAppend));
-
 		globalContainer.insertBefore(imagesContainerTag, footerTag);
+
+		let imagesUrlList = getListUrlImages(data, nbManipulatedImages, nbNotManipulatedImages);
+		createImageCollageLayout(imagesContainerTag, imagesUrlList);
 	});
 }
 
-function createImagesTags(imageArray, nbImages, widthImageCell){
-	imagesCellsTags = [];
-	for(let i=0;i<nbImages;i+=1){
-		let indexImage = randInt(0, imageArray.length);
+function getListUrlImages(data, nbManipulatedImages, nbNotManipulatedImages){
+	let manipulatedImages = data.filter(imageData => imageData.manipulated);
+	let notManipulatedImages = data.filter(imageData => !imageData.manipulated);
 
-		let imageCellTag = document.createElement("div");
-		imageCellTag.classList.add('imgCell');
-		imageCellTag.style.width = widthImageCell + "px";
+	shuffle(manipulatedImages);
+	shuffle(notManipulatedImages);
 
-		let imageTag = document.createElement("img");
-		imageTag.classList.add('exImg');
-		imageTag.style.width = widthImageCell * 0.8 + "px";
-		imageTag.style.maxHeight = imageTag.style.width;
-		imageTag.src = "images/" + imageArray[indexImage].filename;
-		imageTag.manipulated = imageArray[indexImage].manipulated;
-		imageTag.idx = indexImage;
+	let listImages = manipulatedImages.slice(0, nbManipulatedImages).concat(notManipulatedImages.slice(0, nbNotManipulatedImages));
+	shuffle(listImages);
 
-		imageCellTag.appendChild(imageTag);
-		imagesCellsTags.push(imageCellTag);
+	return listImages.map(imageData => imageData.filename);
+}
 
-		let absoluteBox = document.createElement('div');
-		absoluteBox.classList.add('absBox');
-		absoluteBox.style.width = imageCellTag.style.width;
+function getImagesSize(imagesSrc){
+	return new Promise((resolve, reject) => {
+		let nbImagesLoaded = 0;
+		let totalImages = imagesSrc.length;
+		let imagesData = [];
+		function imageLoaded() {
+			nbImagesLoaded++;
+			if (nbImagesLoaded == totalImages) {
+				allImagesLoaded();
+			}
+		}
 
-		// zoomImg and tickImg are squares.
-		let zoomImg = document.createElement('img');
-		zoomImg.classList.add('hoverImg');
-		zoomImg.src = 'assets/zoom-in.png';
-		zoomImg.style.width = widthImageCell * 0.2 + "px";
-		zoomImg.style.height = zoomImg.style.width;
-		zoomImg.style.marginLeft = widthImageCell * 0.2 + "px";
-		let tickImg = document.createElement('img');
-		tickImg.classList.add('hoverImg');
-		tickImg.src = 'assets/tick.png';
-		tickImg.style.width = widthImageCell * 0.2 + "px";
-		tickImg.style.height = tickImg.style.width;
-		tickImg.style.marginLeft = widthImageCell * 0.2 + "px";
-		// functions
-		zoomImg.addEventListener('click', function (e) {
-			let zoomDiv = document.createElement('div');
-			zoomDiv.classList.add('darkFrontDiv')
+		function allImagesLoaded(){
+			resolve(imagesData.map(imageData => ({
+				'url': imageData.url,
+				'width': imageData.imageTag.width,
+				'height': imageData.imageTag.height
+			})));
+		}
 
-			let zoomedPic = document.createElement('img');
-			zoomedPic.src = imageTag.src;
+		for(let imageSrc of imagesSrc){
+			let imageTag = document.createElement("img");
+			imageTag.onload = function(){
+				imageLoaded();
+			};
+			let url = 'images/' + imageSrc;
+			imageTag.src = url;
+			imagesData.push({
+				'url': url,
+				'imageTag': imageTag
+			});
+		}
+	});
+}
 
-			zoomDiv.addEventListener('click', function (e) {
-				document.body.removeChild(zoomDiv);
+function createImageCollageLayout(imagesContainerTag, imagesSrc){
+	let nbImagesColumns = Math.ceil(Math.sqrt(imagesSrc.length));
+	let nbImagesRows = Math.ceil(imagesSrc.length / nbImagesColumns);
+
+	let maxHeightRow = 200;
+	let maxWidthColumn = 200;
+	let gapWidth = 20;
+
+	let minLeftMargin = 100;
+
+	let heightRow = Math.min((imagesContainerTag.clientHeight - (nbImagesRows - 1) * gapWidth) / nbImagesRows, maxHeightRow);
+	let widthColumn = Math.min(((imagesContainerTag.clientWidth - 2 * minLeftMargin) - (nbImagesColumns - 1) * gapWidth) / nbImagesColumns, maxWidthColumn);
+
+	let widthRows = nbImagesColumns * widthColumn + (nbImagesColumns - 1) * gapWidth;
+
+	let leftMargin = (imagesContainerTag.clientWidth - widthRows) / 2;
+
+	getImagesSize(imagesSrc).then(imagesSize => {
+		let imagesTags = imagesSize.map((imageSize, i) => {
+			let columnIdx = i % nbImagesColumns;
+			let rowIdx = Math.floor(i / nbImagesColumns);
+
+			let imageTag = document.createElement("img");
+			imageTag.src = imageSize.url;
+			imageTag.style.position = 'absolute';
+			imageTag.style.top = rowIdx * (heightRow + gapWidth) + 'px';
+			imageTag.style.left = columnIdx * (widthColumn + gapWidth) + leftMargin + 'px';
+			imageTag.style.maxWidth = widthColumn + 'px';
+			imageTag.style.maxHeight = heightRow + 'px';
+
+			imageTag.manipulated = true; // CACA
+
+			imageTag.addEventListener('click', function (e) {
+				imageTag.selectedImage = !imageTag.selectedImage;
+				if (imageTag.selectedImage) {
+					imageTag.style.boxShadow = "0 0 20px 5px rgba(255, 255, 255, 0.7)";
+				} else {
+					imageTag.style.boxShadow = "0 0 20px 5px rgba(0, 0, 0, 0.6)";
+				}
 			});
 
-			zoomDiv.appendChild(zoomedPic);
-			document.body.appendChild(zoomDiv);
+			return imageTag;
 		});
-		tickImg.addEventListener('click', function (e) {
-			imageTag.selectedImage = !imageTag.selectedImage;
-			if (imageTag.selectedImage) {
-				imageTag.style.boxShadow = "0 0 20px 5px rgba(255, 255, 255, 0.7)";
-			} else {
-				imageTag.style.boxShadow = "0 0 20px 5px rgba(0, 0, 0, 0.6)";
-			}
-		});
-		absoluteBox.appendChild(zoomImg);
-		absoluteBox.appendChild(tickImg);
+		imagesTags.forEach(imageTag => imagesContainerTag.appendChild(imageTag));
+	});
+}
 
-		imageCellTag.addEventListener('mouseenter', function (e) {
-			// if outside of the listener, img height is not defined yet --> jquery pls
-			let buttonsTopMargin = imageCellTag.clientHeight / 2  - zoomImg.style.width.slice(0, -2) / 2 + 'px';
-			zoomImg.style.marginTop = buttonsTopMargin;
-			tickImg.style.marginTop = buttonsTopMargin;
-			// ---
-			imageCellTag.appendChild(absoluteBox);
-		});
-		imageCellTag.addEventListener('mouseleave', function (e) {
-			imageCellTag.removeChild(absoluteBox);
-		});
-	}
-	return imagesCellsTags;
+function createImagesTags(imageArray, imagesContainerTag){
+	createImageCollageLayout(imagesContainerTag, imageArray.map(i => i.filename));
 }
 
 function loadData() {
